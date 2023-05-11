6e7c9c2cd71444332f0e23e788f59f4b09225edd
David Andrés Carrero <128672027+Drasticmitten@users.noreply.github.com>
2023-05-11 10:51:52 -0500

    Create vehicle_controller.py

---
+++ controller/vehicle_controller.py
@@ -0,0 +1,31 @@
+import json
+import os
+from logic.vehicle import Vehicle
+PATH = os.getcwd()
+DIR_DATA = PATH + '{0}data{0}'.format(os.sep)
+
+
+class VehicleController(object):
+    def __init__(self):
+        self.file = '{0}{1}'.format(DIR_DATA, 'storage.json')
+
+    def add(self, vehicle: Vehicle = Vehicle()) -> str:
+        with open(self.file, 'r+') as f:
+            data = json.load(f)
+            data['vehicles'].append(vehicle.__dict__)
+            print(vehicle.__str__())
+            f.seek(0)
+            json.dump(data, f)
+        f.close()
+        return vehicle.__str__()
+
+    def show(self):
+        with open(self.file, 'r') as file:
+            json_object = json.load(file)
+        return json_object
+
+    def compare(self, value):
+        with open(self.file) as f:
+            data = json.load(f)
+        vehicles = [vehicle for vehicle in data['vehicles'] if value in str(vehicle.values())]
+        return vehicles
