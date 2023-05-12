47190b8226d6e4ca84885d64976cef710f1af83e
Jeronimo Pinto <124058219+Jeropinto@users.noreply.github.com>
2023-05-12 18:37:40 -0500

    Add files via upload

---
+++ test/test_Operator.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+from logic.address import Address
+from logic.operator import Operator
+
+
+class TestOperator(TestCase):
+    Operator = Operator()
+
+    def test_instance(self):
+        self.assertIsInstance(self.Operator, Operator, "it's a instance")
+
+    def test_authorization(self):
+        self.assertEqual(self.Operator.authorization, 0)
