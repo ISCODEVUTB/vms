b83abd7ebf3e08b2f3de8ec60af0b057505d4a46
David Andrés Carrero <128672027+Drasticmitten@users.noreply.github.com>
2023-05-18 10:58:56 -0500

    Update shopping_cart_controller.py

--- controller/shopping_cart_controller.py
+++ controller/shopping_cart_controller.py
@@ -18,16 +18,3 @@ class ShoppingController(object):
             data = json.load(file)
         shopping_cart = [purchase for purchase in data.values() if value in purchase["dni"]]
         return shopping_cart
-
-    def erase_vehicle(self, dni, id_v):
-        with open(self.file, "r") as file:
-            data = json.load(file)
-        if dni in data:
-            vehicles = data[dni]["vehicles"]
-            updated_vehicles = [vehicle for vehicle in vehicles if vehicle["_id_vehicle"] != int(id_v)]
-            data[dni]["vehicles"] = updated_vehicles
-            with open(self.file, "w") as file:
-                json.dump(data, file, indent=4)
-            return "The vehicle data was deleted correctly"
-        return "The dni doesn't exist"
-
